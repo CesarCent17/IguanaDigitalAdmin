2d96128bf99b2530b7c3dcffd8ba323e978e0727
CesarCent <centurion_cesar1234@hotmail.com>
2022-08-30 20:24:28 -0500

    se agrega Usuarios.html con una tabla de ejemplo, se modifica el login.js y queda a revision

--- js/login.js
+++ js/login.js
@@ -1,10 +1,14 @@
 async function login(){
-    alert("Funcion Login")
+    // alert("Funcion Login")
  
     let email = document.getElementById('EmailTxt').value;
     let password = document.getElementById('PasswordTxt').value;
-    alert(email);
-    alert(password);
+    if(email == '' || password == ''){
+        alert("Error debe llenar los campos")
+        return;
+    }
+    // alert(email);
+    // alert(password);
 
     let url = 'http://10.35.3.16:9898/api/es/usuario/v1/'+email+'/'+password
     alert(url);
@@ -18,4 +22,9 @@ async function login(){
     usuario = await request.json();
     let mensaje = "El usuario "+usuario.nombreCompleto+" ha iniciado sesion!"
     alert(mensaje);
+    if(request.ok){
+        window.location.href = 'usuarios.html'
+    } else if(request.status == 500) {
+        alert("Las credenciales son incorrectas");
+    }
 }
