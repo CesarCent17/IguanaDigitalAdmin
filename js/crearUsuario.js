15249de0621bc31d181a8ab99558b7dd69574f82
CesarCent <centurion_cesar1234@hotmail.com>
2022-09-07 01:04:10 -0500

    Modal crearUsuario

--- js/crearUsuario.js
+++ js/crearUsuario.js
@@ -1,21 +1,25 @@
-async function crearUsuari(){
+async function crearUsuari() {
     let obj = {};
     obj.nombreCompleto = document.getElementById('NombreTxt').value
     obj.email = document.getElementById('EmailTxt').value
     obj.estado = document.getElementById('EstadoTxt').value
     obj.contrasenia = document.getElementById('PasswordTxt').value
 
-
-    const res = await fetch('https://appgizlorecords.herokuapp.com/api/es/usuario/v1', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(obj)
-    });
-    if(res.status >= 300){
-        alert("Error al registrar el usuario, por favor intentelo mas tarde:)")
+    if (obj.nombreCompleto != "" && obj.email != "" && obj.estado != "" && obj.contrasenia != "") {
+        const res = await fetch('https://appgizlorecords.herokuapp.com/api/es/usuario/v1', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        });
+        if (res.status >= 300) {
+            alert("Error al registrar el usuario, por favor intentelo mas tarde:)")
+        }
+        alert("El usuario fue registrado con exito!")
+        location.reload();
+    } else {
+        alert("Por favor debe llenar los campos!")
     }
-    alert("El usuario fue registrado con exito!")
 }
